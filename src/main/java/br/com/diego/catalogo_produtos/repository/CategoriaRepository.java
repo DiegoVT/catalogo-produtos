0178cbf0b4ea765f41207b1183e9d6720d26b0bb
DiegoVT <diego.linguado@hotmail.com>
2026-02-03 18:07:59 -0300

    Ajusta CategoriaRepository e adiciona configurações

--- src/main/java/br/com/diego/catalogo_produtos/repository/CategoriaRepository.java
+++ src/main/java/br/com/diego/catalogo_produtos/repository/CategoriaRepository.java
@@ -4,3 +4,5 @@ import br.com.diego.catalogo_produtos.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
+
+
